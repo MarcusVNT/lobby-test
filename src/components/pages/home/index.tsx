e72e27f97af03edd1e7629b9071cf586a3165017
Marcus Vinícius <marcus-vnt@live.com>
2024-11-30 23:16:08 -0300

    Style adjustments.

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button, Stack, Typography } from '@mui/material'
 import Grid from '@mui/material/Grid2'
 import Logo from '/public/images/logo.svg'
@@ -6,75 +8,58 @@ import Point from '/public/images/point.svg'
 
 export default function Home() {
   return (
-    <Stack
-      sx={{
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '100vh',
-      }}
-    >
+    <Stack alignItems="center" justifyContent="center" minHeight="100vh">
       <Stack
-        sx={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#fff',
-          borderRadius: '24px',
-        }}
+        alignItems="center"
+        justifyContent="center"
+        borderRadius={'24px'}
+        bgcolor="#fff"
+        padding="126px 0 0 0"
+        textAlign="center"
+        maxWidth="1024px"
+        width="100%"
       >
-        <Stack
-          sx={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: '126px 0 88px 0',
-            textAlign: 'center',
-          }}
-        >
-          <Grid container>
-            <Grid size={12} marginBottom="48px">
-              <Logo width={189} height={54} />
-            </Grid>
-            <Grid size={12}>
-              <Stack
-                gap="20px"
-                textAlign="center"
-                maxWidth="436px"
-                mx="auto"
-                marginBottom="40px"
-              >
-                <Typography variant="h1" fontSize="2.5rem">
-                  Bem vindo!
-                </Typography>
-                <Typography
-                  sx={{
-                    color: 'primary.light',
-                    fontSize: '1.125rem',
-                  }}
-                >
-                  Estamos muito felizes em ter você em nossa equipe! Preencha as
-                  perguntinhas a seguir para escolher o seu presente! 🎁
-                </Typography>
-              </Stack>
-            </Grid>
-            <Grid size={12}>
-              <Button variant="contained" color="primary">
-                Começar
-              </Button>
-            </Grid>
+        <Grid container>
+          <Grid size={12} marginBottom="48px">
+            <Logo width={189} height={54} />
           </Grid>
-        </Stack>
+          <Grid size={12}>
+            <Stack
+              gap="20px"
+              textAlign="center"
+              maxWidth="436px"
+              mx="auto"
+              marginBottom="40px"
+            >
+              <Typography variant="h1" fontSize="2.5rem">
+                Bem vindo!
+              </Typography>
+              <Typography color="primary.light" fontSize="1.125rem">
+                Estamos muito felizes em ter você em nossa equipe! Preencha as
+                perguntinhas a seguir para escolher o seu presente! 🎁
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid size={12}>
+            <Button variant="contained" color="primary">
+              Começar
+            </Button>
+          </Grid>
+        </Grid>
         <Stack
           flexDirection="row"
           alignItems="center"
           justifyContent="center"
           gap="8px"
+          marginTop="88px"
           marginBottom="20px"
         >
           <Vector />
           <Point width="2px" />
-          <Typography fontWeight="600" fontSize=".875rem" alignItems="center">
+          <Typography fontWeight="600" fontSize=".875rem">
             2024
           </Typography>
-          <Typography fontWeight="600" fontSize=".875rem" alignItems="center">
+          <Typography fontWeight="600" fontSize=".875rem">
             Lobby.tech{' '}
             <Typography component="span" fontSize=".875rem">
               em parceria com a
